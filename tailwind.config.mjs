b515e979d83989552c8970aaf3d7af87384cc85f
LopezDev <aragonlopezjuan87@gmail.com>
2024-04-06 19:38:52 +0200

    all section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,12 +10,16 @@ export default {
 				"h1Desktop": ['56px', { lineHeight: '1' }],
 				"h1Tablet": ['52px', { lineHeight: '1' }],
 				"h1Mobile": ['40px', { lineHeight: '1' }],
+				"h2Desktop": ['48px', { lineHeight: '1' }],
+				"h2Tablet": ['44px', { lineHeight: '1' }],
+				"h2Mobile": ['36px', { lineHeight: '1' }],
 				lg: ['24px', { lineHeight: '1.75rem' }],
 			},
 			textColor: {
 				"black": "#464646",
 				"primary": '#2563EB',
-				"secondary": '#EBAF26'
+				"secondary": '#EBAF26',
+				"secondary-200": "#F1C45D"
 			},
 			backgroundColor: {
 				"bg": "#FFFDFA",
